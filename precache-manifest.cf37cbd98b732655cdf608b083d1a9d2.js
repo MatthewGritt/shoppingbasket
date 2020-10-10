cfdc5134f9a412efda8b620aae689fc4d069cbf3
Matt <matthew.gritt@yahoo.co.uk>
2020-10-10 15:27:23 +0100

    Updates

---
+++ precache-manifest.cf37cbd98b732655cdf608b083d1a9d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc5e9a2c9be6737126b6f9b0c9cb5a1c",
+    "url": "/shoppingbasket/index.html"
+  },
+  {
+    "revision": "655c6fb11e5a8d5c1689",
+    "url": "/shoppingbasket/static/css/main.d535fa29.chunk.css"
+  },
+  {
+    "revision": "431c595df17a329f3a0c",
+    "url": "/shoppingbasket/static/js/2.6d253f5e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/shoppingbasket/static/js/2.6d253f5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "655c6fb11e5a8d5c1689",
+    "url": "/shoppingbasket/static/js/main.929f9f12.chunk.js"
+  },
+  {
+    "revision": "8084a5ffc4c9cf18fc3d",
+    "url": "/shoppingbasket/static/js/runtime-main.fd1c8caa.js"
+  },
+  {
+    "revision": "9e294c66424e75a049af4dd364bd7dc7",
+    "url": "/shoppingbasket/static/media/book.9e294c66.webp"
+  },
+  {
+    "revision": "f27473bccf84cb312276627489720e6f",
+    "url": "/shoppingbasket/static/media/game.f27473bc.webp"
+  },
+  {
+    "revision": "d1f379789a165006a20ad19a369bfa08",
+    "url": "/shoppingbasket/static/media/tv.d1f37978.webp"
+  },
+  {
+    "revision": "e8bd0699af2f7ca7925fe05325ad4cb3",
+    "url": "/shoppingbasket/static/media/watch.e8bd0699.webp"
+  }
+]);
